b77652dc2c11dec3fb380bfcecda1a98e5bb9862
Alexis Martinez <115680147+AlexisTech41@users.noreply.github.com>
2024-02-08 17:39:32 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'fsstienda.1.us-1.fl0.io'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
